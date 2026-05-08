778634f17cb2949e5b89ff37a34e0137e0447809
Anuj Kumar <xp27anujkumar@gmail.com>
2026-05-08 14:08:24 +0530

    add cors inside backend server structure and initailize frontend React application with routing

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ const PORT = process.env.PORT || 4500;
 // Connect db
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/login" element={<h1>Login</h1>} />
+      <Route path="/register" element={<h1>Register</h1>} />
+      <Route path="/bookmarks" element={<h1>Bookmarks</h1>} />
+    </Routes>
   );
 }
 
