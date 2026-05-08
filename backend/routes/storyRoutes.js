44fcb129ae8f7eab8e071ea11c30bd8c3a865ba0
Anuj Kumar <xp27anujkumar@gmail.com>
2026-05-08 11:48:11 +0530

    Protected bookmark route with authentication middleware

--- backend/routes/storyRoutes.js
+++ backend/routes/storyRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getAllStories, getStoryById, toggleBookmark } from "../controllers/storyController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getAllStories);
 router.get("/:id", getStoryById);
-router.post("/:id/bookmark", toggleBookmark);
+router.post("/:id/bookmark", authMiddleware, toggleBookmark);
 
 export default router;
